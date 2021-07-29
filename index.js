0561742beefd80b4a55627563f73a6615f50adec
Mark Kaylor <mark.kaylor@strapi.io>
2021-07-29 14:51:31 +0200

    move config

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const { statement } = j.template;
 
 const SERVER_DIRECTORIES = [
   "controllers",
-  "middlewares",
   "models",
+  "middlewares",
   "services",
   "hooks",
 ];
@@ -29,32 +29,12 @@ async function migratePlugin(pluginPath) {
 
     // move server files to /src/server
     for (const directory of SERVER_DIRECTORIES) {
-      const exists = await fs.pathExists(join(plugin, directory));
-      if (!exists) continue;
-
-      await fs.move(
-        join(plugin, directory),
-        join(plugin, "src", "server", directory)
-      );
-
-      await createDirectoryIndex(join(plugin, "src", "server", directory));
+      await moveDirectory(plugin, directory);
     }
 
-    // handle config
-    const pluginConfig = join(plugin, "config");
-    await fs.move(
-      join(pluginConfig, "functions", "bootstrap.js"),
-      join(plugin, "src", "server", "bootstrap.js")
-    );
-    await fs.remove(join(pluginConfig, "functions"))
-    const configDirectory = await fs.readdir(join(plugin, "config"));
-    console.log(configDirectory)
-
-    await fs.copy(
-      "./utils/module-exports.js",
-      join(plugin, "src", "server", "index.js")
-    );
-
+    await moveBootstrapFunction(plugin);
+    await moveRoutes(plugin)
+    await createServerIndex(join(plugin, "src", "server"));
     // move admin files to /src
     await fs.move(join(plugin, "admin", "src"), join(plugin, "src", "admin"));
     await fs.remove(join(plugin, "admin"));
@@ -63,20 +43,71 @@ async function migratePlugin(pluginPath) {
   }
 }
 
-// getModels
-// getControllers
-// getBootstrap
-// getServices
-// getHooks
+async function moveRoutes(pluginPath) {
+  
+  const routes = join(pluginPath, "config", "routes.json")
+  await fs.move(
+    routes,
+    join(pluginPath, "src", "server", "routes.json")
+  );
+}
+
+async function moveBootstrapFunction(pluginPath) {
+  const bootstrapOrigin = join(
+    pluginPath,
+    "config",
+    "functions",
+    "bootstrap.js"
+  );
+
+  await fs.move(
+    bootstrapOrigin,
+    join(pluginPath, "src", "server", "bootstrap.js")
+  );
+
+  const functionsDir = join(pluginPath, "config", "functions");
+  const dirContent = await fs.readdir(functionsDir);
+
+  if (!dirContent.length) {
+    await fs.remove(functionsDir);
+  }
+}
+
+async function moveDirectory(origin, destination) {
+  const hasDir = await fs.pathExists(join(origin, destination));
+  if (!hasDir) return;
+
+  const serverPath = join(origin, "src", "server");
+  await fs.move(join(origin, destination), join(serverPath, destination));
+
+  await createDirectoryIndex(join(serverPath, destination));
+}
+
+async function createServerIndex(serverDir) {
+  const indexPath = join(serverDir, "index.js");
+  await fs.copy(join(__dirname, "utils", "module-exports.js"), indexPath);
+
+  const dirContent = await fs.readdir(serverDir);
+  console.log(dirContent)
+  const filesToImport = dirContent.filter((file) => file !== "index.js");
+  
+  await addImportsToFile(indexPath, filesToImport);
+  await addModulesToExport(indexPath, filesToImport);
+}
 
 async function createDirectoryIndex(dir) {
+  const hasDir = await fs.pathExists(dir);
+  if (!hasDir) return;
   // get all files from the dir
-  const filesToImport = await fs.readdir(dir);
+  const dirContent = await fs.readdir(dir);
 
   const indexPath = join(dir, "index.js");
   // create index.js for dir
   await fs.copy(join(__dirname, "utils", "module-exports.js"), indexPath);
+
   // import all files to dir
+  const filesToImport = dirContent.filter((file) => file.includes(".js"));
+
   await addImportsToFile(indexPath, filesToImport);
   await addModulesToExport(indexPath, filesToImport);
 }
@@ -97,7 +128,8 @@ async function addImportsToFile(filePath, imports) {
   const body = root.find(j.Program).get("body");
 
   imports.forEach((fileImport) => {
-    const filename = fileImport.replace(".js", "");
+    // TODO: fix this shit
+    const filename = fileImport.split(".").slice(0, 1)
 
     const declaration = statement`const ${camelCase(
       filename
@@ -105,16 +137,17 @@ async function addImportsToFile(filePath, imports) {
 
     const hasUseStrict = body.get(0).value.directive === "use strict";
     if (hasUseStrict) {
-      // When use strict is present import after
+      // When use strict is present add imports after
       body.get(0).insertAfter(declaration);
     } else {
-      //  Otherwise add them to the top of the file
+      // Otherwise add them to the top of the file
       body.unshift(declaration);
     }
   });
 
   await fs.writeFile(filePath, root.toSource());
 }
+
 async function addModulesToExport(filePath, modules) {
   const fileContent = await fs.readFile(filePath);
   const file = fileContent.toString();
@@ -130,21 +163,21 @@ async function addModulesToExport(filePath, modules) {
     },
   });
 
-  modules.forEach((module) => {
-    const moduleName = module.replace(".js", "");
-    // Get all varaiable declarations that are importing mo
+  modules.forEach((mod) => {
+    // TODO: fix this shit
+    const moduleName = mod.split(".").slice(0, 1)
+    const property = j.property(
+      "init",
+      j.identifier(camelCase(moduleName)),
+      j.identifier(camelCase(moduleName))
+    );
+
     moduleExports
       .get()
-      .value.right.properties.push(
-        j.property(
-          "init",
-          j.identifier(camelCase(moduleName)),
-          j.identifier(camelCase(moduleName))
-        )
-      );
+      .value.right.properties.push({ ...property, shorthand: true });
   });
 
   await fs.writeFile(filePath, root.toSource());
 }
 
-migratePlugin("../../plugins/strapi-plugin-stripe-payment");
+migratePlugin("../plugin-tests/strapi-plugin-content-manager");
