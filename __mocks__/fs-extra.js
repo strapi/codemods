386f050cbcb21c5e8b5e27141fe267273461cad2
markkaylor <mark.kaylor@strapi.io>
2022-01-21 10:34:01 +0100

    add unit tests (#10)

---
+++ __mocks__/fs-extra.js
@@ -0,0 +1,16 @@
+const fsExtra = require('fs-extra');
+
+jest.mock('fs-extra', () => ({
+  exists: jest.fn(() => Promise.resolve()),
+  pathExists: jest.fn(() => Promise.resolve()),
+  readdir: jest.fn(() => Promise.resolve()),
+  readJSON: jest.fn(() => Promise.resolve()),
+  remove: jest.fn(() => Promise.resolve()),
+  ensureFile: jest.fn(() => Promise.resolve()),
+  writeJSON: jest.fn(() => Promise.resolve()),
+  copy: jest.fn(() => Promise.resolve()),
+  move: jest.fn(() => Promise.resolve()),
+  createWriteStream: jest.fn(),
+}));
+
+module.exports = fsExtra;
